c9cbe8d5cbe5b10c5a419f64e0232c9ea23f5f28
1PhuongT <phuong.tieu@optusnet.com.au>
2022-08-01 17:00:52 +1000

    Added TVS, transform

---
+++ DB_config.py
@@ -0,0 +1,5 @@
+# DB config file
+
+DB_NAME = 'employee_db'
+DB_USER = 'phuong.tieu@optus.com.au'
+DB_PWD = 'GoodPT168!'
---
+++ Resources/DB_config.py
@@ -0,0 +1,4 @@
+# DB config file
+
+DB_NAME = 'AFLGame_db'
+DB_PWD = '0000' # enter your password
